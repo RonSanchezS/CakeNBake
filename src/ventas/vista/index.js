68b5ff4bdb20e531d3f42381132fe42c7bf75844
Andres-Quintanilla <88890587+Andres-Quintanilla@users.noreply.github.com>
2024-02-09 00:05:53 -0400

    Funcionalidad para ordenar todos los campos de Venta
    Se la función cambiarOrden en ventas para poder ordenar de manera asc y des los datos de la tabla de ventas

--- src/ventas/vista/index.js
+++ src/ventas/vista/index.js
@@ -22,7 +22,7 @@ connection.connect((err) => {
   console.log("Conexión a la base de datos establecida");
 
   connection.query(
-    "select c.id, c.nombre, p.precio, COALESCE(p.fecha, '') as fecha, COALESCE(p.fecha_entrega, '') as fecha_entrega, COALESCE(p.detalle, '') as detalle, p.metodo_pago from ventas p join cliente c where c.id = p.id_usuario order by fecha desc" /*ver esto*/,
+    "select c.id, c.nombre, p.precio, COALESCE(p.fecha, '') as fecha, COALESCE(p.fecha_entrega, '') as fecha_entrega, COALESCE(p.detalle, '') as detalle, p.metodo_pago from ventas p join cliente c where c.id = p.id_usuario order by fecha desc",/*ver esto*/
     (error, results, fields) => {
       if (error) {
         console.error("Error al realizar la consulta:", error);
@@ -86,7 +86,8 @@ document.addEventListener("DOMContentLoaded", () => {
             return 1; // Cambiado a 1 para ordenar de forma descendente
           }
           return 0;
-        });
+        }
+        );
         updateTableBody(ventasPorDetalle);
         // Lógica para ordenar por Detalle
         break;
@@ -137,8 +138,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
- 
+  const getData = () => {
+    return todasLasVentas;
+  };
+  const getData2 = () => {
+    return new Promise((resolve, reject) => {
+      const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "pasteleria",
+      });
+
+      connection.connect((err) => {
+        if (err) {
+          console.error("Error al conectar a la base de datos:", err);
+          reject(err);
+          return;
+        }
+        console.log("Conexión a la base de datos establecida");
+
+        connection.query("select c.id, c.nombre, p.precio, COALESCE(p.fecha, '') as fecha, COALESCE(p.fecha_entrega, '') as fecha_entrega, COALESCE(p.detalle, '') as detalle, p.metodo_pago from ventas p join cliente c where c.id = p.id_usuario order by fecha_entrega desc", (error, results, fields) => {
+          if (error) {
+            console.error("Error al realizar la consulta:", error);
+            reject(error);
+            return;
+          }
+          if (results.length > 0) {
+            resolve(results);
+          } else {
+            resolve([]);
+          }
+        });
+        connection.end();
+      });
+    });
+  };
 });
+const getData = () => {
+  return todasLasVentas;
+};
 const getData2 = () => {
   return new Promise((resolve, reject) => {
     const connection = mysql.createConnection({
@@ -156,25 +195,22 @@ const getData2 = () => {
       }
       console.log("Conexión a la base de datos establecida");
 
-      connection.query(
-        "select c.id, c.nombre, p.precio, COALESCE(p.fecha, '') as fecha, COALESCE(p.fecha_entrega, '') as fecha_entrega, COALESCE(p.detalle, '') as detalle, p.metodo_pago from ventas p join cliente c where c.id = p.id_usuario order by fecha_entrega desc",
-        (error, results, fields) => {
-          if (error) {
-            console.error("Error al realizar la consulta:", error);
-            reject(error);
-            return;
-          }
-          if (results.length > 0) {
-            resolve(results);
-          } else {
-            resolve([]);
-          }
-        });
-        connection.end();
+      connection.query("select c.id, c.nombre, p.precio, COALESCE(p.fecha, '') as fecha, COALESCE(p.fecha_entrega, '') as fecha_entrega, COALESCE(p.detalle, '') as detalle, p.metodo_pago from ventas p join cliente c where c.id = p.id_usuario order by fecha_entrega desc", (error, results, fields) => {
+        if (error) {
+          console.error("Error al realizar la consulta:", error);
+          reject(error);
+          return;
+        }
+        if (results.length > 0) {
+          resolve(results);
+        } else {
+          resolve([]);
+        }
       });
+      connection.end();
     });
-  };
-});
+  });
+};
 
 function updateTableBody(ventas) {
   const tbody = document.querySelector("#tbody-ventas");
@@ -206,4 +242,137 @@ function cambiarOrden(columnId) {
     flecha.classList.remove("up");
     flecha.classList.add("down");
   }
+  switch (columnId) {
+    case "pago":
+      let ventasPorMetodoPago = getData();
+      if (flechaAbajo) {
+        ventasPorMetodoPago = ventasPorMetodoPago.sort((a, b) => {
+          const metodoPagoA = a.metodo_pago.toLowerCase();
+          const metodoPagoB = b.metodo_pago.toLowerCase();
+
+          return metodoPagoA.localeCompare(metodoPagoB);
+        });
+      } else {
+        ventasPorMetodoPago = ventasPorMetodoPago.sort((a, b) => {
+          const metodoPagoA = a.metodo_pago.toLowerCase();
+          const metodoPagoB = b.metodo_pago.toLowerCase();
+
+          return metodoPagoB.localeCompare(metodoPagoA);
+        });
+      }
+      updateTableBody(ventasPorMetodoPago);
+      break;
+    case "precio":
+      let ventasPorPrecio = getData();
+      if (flechaAbajo) {
+        ventasPorPrecio = ventasPorPrecio.sort((a, b) => b.precio - a.precio); // Ordenar de forma descendente
+      } else {
+        ventasPorPrecio = ventasPorPrecio.sort((a, b) => a.precio - b.precio); // Ordenar de forma ascendente
+      }
+      updateTableBody(ventasPorPrecio);
+      break;
+    case "detalle":
+      let ventasPorDetalle = getData();
+      if (flechaAbajo) {
+        ventasPorDetalle = ventasPorDetalle.sort((a, b) => {
+          if (a.detalle > b.detalle) {
+            return -1; // Ordenar de forma descendente
+          }
+          if (a.detalle < b.detalle) {
+            return 1; // Ordenar de forma descendente
+          }
+          return 0;
+        });
+      } else {
+        ventasPorDetalle = ventasPorDetalle.sort((a, b) => {
+          if (a.detalle > b.detalle) {
+            return 1; // Ordenar de forma ascendente
+          }
+          if (a.detalle < b.detalle) {
+            return -1; // Ordenar de forma ascendente
+          }
+          return 0;
+        });
+      }
+      updateTableBody(ventasPorDetalle);
+
+      break;
+    case "fechaP":
+      let ventasPorFechaPedido = getData();
+      if (flechaAbajo) {
+        ventasPorFechaPedido = ventasPorFechaPedido.sort((a, b) => {
+          const dateA = new Date(a.fecha);
+          const dateB = new Date(b.fecha);
+
+          // Manejo de elementos sin fecha
+          if (isNaN(dateA) && isNaN(dateB)) {
+            return 0; // Si ambos no tienen fecha, no cambian su orden relativo
+          } else if (isNaN(dateA)) {
+            return 1; // Si a no tiene fecha, a va al final
+          } else if (isNaN(dateB)) {
+            return -1; // Si b no tiene fecha, b va al final
+          } else {
+            return dateB - dateA; // Ordenamiento descendente por fecha
+          }
+        });
+      } else {
+        ventasPorFechaPedido = ventasPorFechaPedido.sort((a, b) => {
+          const dateA = new Date(a.fecha);
+          const dateB = new Date(b.fecha);
+
+          // Manejo de elementos sin fecha
+          if (isNaN(dateA) && isNaN(dateB)) {
+            return 0; // Si ambos no tienen fecha, no cambian su orden relativo
+          } else if (isNaN(dateA)) {
+            return -1; // Si a no tiene fecha, a va al inicio
+          } else if (isNaN(dateB)) {
+            return 1; // Si b no tiene fecha, b va al inicio
+          } else {
+            return dateA - dateB; // Ordenamiento ascendente por fecha
+          }
+        });
+      }
+      updateTableBody(ventasPorFechaPedido);
+
+      break;
+    case "fechaE":
+      getData2().then((results) => {
+        if (flechaAbajo) {
+          results = results.sort((a, b) => a.id - b.id); // Ordenar de forma ascendente
+        } else {
+          results = results.sort((a, b) => b.id - a.id); // Ordenar de forma descendente
+        }
+        updateTableBody(results);
+      });
+
+      break;
+    case "id":
+      let ventasPorID = getData();
+      if (flechaAbajo) {
+        ventasPorID = ventasPorID.sort((a, b) => a.id - b.id); // Ordenar de forma ascendente
+      } else {
+        ventasPorID = ventasPorID.sort((a, b) => b.id - a.id); // Ordenar de forma descendente
+      }
+      updateTableBody(ventasPorID);
+      break;
+    case "nombre":
+      let ventasPorNombre = getData();
+      if (flechaAbajo) {
+        ventasPorNombre = ventasPorNombre.sort((a, b) => {
+          const nombreA = a.nombre.toLowerCase();
+          const nombreB = b.nombre.toLowerCase();
+          return nombreA.localeCompare(nombreB);
+        });
+      } else {
+        ventasPorNombre = ventasPorNombre.sort((a, b) => {
+          const nombreA = a.nombre.toLowerCase();
+          const nombreB = b.nombre.toLowerCase();
+          return nombreB.localeCompare(nombreA);
+        });
+      }
+      updateTableBody(ventasPorNombre);
+      break;
+  }
+
+
 }
